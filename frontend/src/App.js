3f34d3d28bba99e38ace18b9e364fb18120db1ff
sanaahamliri <sanahamlirifr01@gmail.com>
2025-03-09 14:51:18 +0000

    feat: setup basic routing with React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,21 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import logo from './logo.svg';
-import './App.css';
-
-// Components
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-
-// Pages
-import Home from './pages/Home';
-import About from './pages/About';
-import Search from './pages/Search';
+import MainLayout from './layouts/MainLayout';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Home from './pages/Home';
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen flex flex-col">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
     </Router>
   );
 }
